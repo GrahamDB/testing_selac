fa250b2365a3c99dcad64aeaef585e0b554b8b31
Graham Derryberry <g.derryberry@gmail.com>
2018-06-26 18:03:32 -0400

    Make selac install a little more flexible

--- setup.R
+++ setup.R
@@ -4,7 +4,7 @@ source(".Rprofile")
 dep_imports<-strsplit("expm, MASS, parallel, phangorn, seqinr, statmod, zoo, RColorBrewer",", ")[[1]]
 install.packages(c("ape","deSolve","nloptr","deSolve","nnet"), 
                  type="source", INSTALL_opts="--with-keep.source")
-install.packages("/home/asterion/git/selac_1.6.1.tar.gz" , repos=NULL,
+install.packages(path.expand(file.path("~","git","selac_1.6.1.tar.gz")) , repos=NULL,
                  type="source", INSTALL_opts="--with-keep.source")
 
 install.packages(dep_imports,type="source", INSTALL_opts="--with-keep.source")
