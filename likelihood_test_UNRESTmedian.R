1a16f7d01748ec68732b043c9dbf8a359b3fdedc
Graham Derryberry <g.derryberry@gmail.com>
2018-06-28 14:40:50 -0400

    Separate tests into scripts for comparison

---
+++ likelihood_test_UNRESTmedian.R
@@ -0,0 +1,22 @@
+## Test 3
+set.seed(4)
+tree <- read.tree("rokasYeast.tre")
+phy <- drop.tip(tree, "Calb")
+yeast.gene <- read.dna("gene1Yeast.fasta", format="fasta")
+yeast.gene <- as.list(as.matrix(cbind(yeast.gene))[1:7,])
+chars <- selac:::DNAbinToCodonNumeric(yeast.gene)
+codon.data <- chars[phy$tip.label,]
+aa.data <- selac:::ConvertCodonNumericDataToAAData(codon.data, numcode=1)
+aa.optim <- apply(aa.data[, -1], 2, selac:::GetMaxName) #starting values for all, final values for majrule
+aa.optim.full.list <- aa.optim
+codon.freq.by.aa <- selac:::GetCodonFreqsByAA(codon.data[,-1], aa.optim, numcode=1)
+codon.freq.by.gene <- selac:::GetCodonFreqsByGene(codon.data[,-1])
+aa.optim.frame.to.add <- matrix(c("optimal", aa.optim), 1, dim(codon.data)[2])
+colnames(aa.optim.frame.to.add) <- colnames(codon.data)
+codon.data <- rbind(codon.data, aa.optim.frame.to.add)
+codon.data <- selac:::SitePattern(codon.data, includes.optimal.aa=TRUE)
+aa.optim = codon.data$optimal.aa
+codon.index.matrix <- selac:::CreateCodonMutationMatrixIndex()
+selac_gamma <- selac:::GetLikelihoodSAC_CodonForManyCharGivenAllParams(log(c(4*4e-7*.5*5e6, 1.829272, 0.101799, .25, .25, .25, rep(1,5), 5)), codon.data, phy, aa.optim_array=aa.optim, codon.freq.by.aa=codon.freq.by.aa, codon.freq.by.gene=codon.freq.by.gene, numcode=1, diploid=TRUE, aa.properties=NULL, volume.fixed.value=0.0003990333, nuc.model="GTR", codon.index.matrix, include.gamma=TRUE, gamma.type="median", ncats=4, k.levels=0, logspace=TRUE, verbose=FALSE)
+comparison <- identical(round(selac_gamma, 3), -6999.538)
+print(comparison)
