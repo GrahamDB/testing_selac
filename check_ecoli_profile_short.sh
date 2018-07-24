ee522ae7967aea135294b531db72bb5ce36b3b87
Graham Derryberry <g.derryberry@gmail.com>
2018-07-24 11:33:09 -0400

    Ignoring data output, adding new scripts

---
+++ check_ecoli_profile_short.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+echo "$0" "$@"
+prof_mode=SHORT
+sole_SEED=$1
+shift
+echo args "$@"
+gamma_mode="none"
+nuc_mode="UNREST"
+codon_mode="selac"
+sys_args="mode=\"${prof_mode}\", nuc.model=\"${nuc_mode}\", gamma.type=\"${gamma_mode}\", nCores=1"
+out_file="log_profile_ecoli${prof_mode}.txt"
+profile_run () {
+  ( date;
+time R -q --no-save -e 'source("R_profile_helper.R", keep.source = T); run_ecoli_profile_mode(seed='$1', '"$sys_args"', ref="'"$2"'")';
+date ;) |& tee -a "${out_file}"
+sleep 5;
+}
+while [[ "$#" -gt 0 ]] do
+  profile_run "${sole_SEED}" "$1"
+  shift 1
+done
