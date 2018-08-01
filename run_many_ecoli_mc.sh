1ee409a5882c89811ca3afde29ad57f7c16bd3e8
Graham Derryberry <g.derryberry@gmail.com>
2018-08-01 12:40:25 -0400

    Early selac UNREST_lognormal benchmarks and draft mc benchmark scripts

---
+++ run_many_ecoli_mc.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+echo "$0" "$@"
+prof_mode=$1
+shift 1
+echo args "$@"
+start=$1
+stop=${2:-$start}
+shift
+shift
+echo args "$@"
+num_cores="${1:-2}"
+[[ -z ${num_cores} ]] && num_cores="2"
+shift 1
+echo args "$@"
+gamma_mode="${1:-quadrature}"
+[[ -z ${gamma_mode} ]] && gamma_mode="quadrature"
+shift 1
+echo args "$@"
+nuc_mode="${1:-UNREST}"
+[[ -z ${nuc_mode} ]] && nuc_mode="UNREST"
+shift 1
+echo args "$@"
+selac_version="${1:-9e90977}"
+[[ -z ${selac_version} ]] && selac_version="9e90977"
+shift 1
+echo args "$@"
+codon_mode="selac"
+sys_args="mode=\"${prof_mode}\", nuc.model=\"${nuc_mode}\", gamma.type=\"${gamma_mode}\", nCores=${num_cores}"
+out_file="log_profile_ecoli${prof_mode}.txt"
+profile_run () {
+  ( date;
+time R -q --no-save -e 'source("R_profile_helper.R", keep.source = T); run_ecoli_profile_mode(seed='$1', '"$sys_args"', ref="'"$2"'")';
+date ;) |& tee -a "${out_file}"
+sleep 5;
+}
+profile_run_seq () {
+  profile_run "$1" "$selac_version"
+#  profile_run "$1" "$3"
+#  profile_run "$1" "$4"
+}
+profile_run_combo () {
+  seed_offset="$1"
+  rA="9e90977"
+  rB="8f13a5a"
+  rC="38a4c36"
+  profile_run_seq "$((seed_offset+0))" #"$rA" "$rB" "$rC"
+  profile_run_seq "$((seed_offset+3))" #"$rB" "$rC" "$rA"
+  profile_run_seq "$((seed_offset+4))" #"$rC" "$rA" "$rB"
+  profile_run_seq "$((seed_offset+1))" #"$rA" "$rC" "$rB"
+  profile_run_seq "$((seed_offset+5))" #"$rC" "$rB" "$rA"
+  profile_run_seq "$((seed_offset+2))" #"$rB" "$rA" "$rC"
+}
+seq $start 6 $stop | while read iter_seed; do profile_run_combo "$iter_seed"; done
+
+#( date;
+#time R -q --no-save -e 'source("R_profile_helper.R", keep.source = T); run_ecoli_profile_mode(seed='$iter_seed', '"$sys_args"', ref="dd94866")';
+#date ;) |& tee -a "${out_file}";
+#sleep 30;
+#( date;
+#time R -q --no-save -e 'source("R_profile_helper.R", keep.source = T); run_ecoli_profile_mode(seed='$iter_seed', '"$sys_args"', ref="744c6c8")';
+#date ;) |& tee -a "${out_file}";
+#sleep 30;
+#( date;
+#time R -q --no-save -e 'source("R_profile_helper.R", keep.source = T); run_ecoli_profile_mode(seed='$iter_seed', '"$sys_args"')';
+#date ;) |& tee -a "${out_file}"
+#sleep 30;
+#done
